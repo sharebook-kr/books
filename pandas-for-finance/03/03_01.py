6f5d72e13a16e62a27335c958d9569c0d1c6accd
Brayden Jo <brayden.jo@outlook.com>
2019-06-21 22:41:50 +0900

    pandas for finance

---
+++ pandas-for-finance/03/03_01.py
@@ -0,0 +1,11 @@
+from pandas import Series
+
+data = [1000, 2000, 3000]
+index = ["메로나", "구구콘", "하겐다즈"]
+s = Series(data=data, index=index)
+
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[-1])
+print(s["하겐다즈"])
