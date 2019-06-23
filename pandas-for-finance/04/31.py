f6e5af140684a7bd0f46dac1283ed45145aceb52
Brayden Jo <brayden.jo@outlook.com>
2019-06-23 16:02:03 +0900

    pandas for finance

---
+++ pandas-for-finance/04/31.py
@@ -0,0 +1,21 @@
+from pandas import DataFrame
+import numpy as np
+
+data = [
+    ["037730", "3R", '1,510'],
+    ["036360", "3SOFT", '1,790'],
+    ["005760", "ACTS", '1,185'],
+]
+
+def remove_comma(x):
+    return x.replace(',', '')
+
+columns = ["종목코드", "종목명", "현재가"]
+df = DataFrame(data=data, columns=columns)
+df['현재가'] = df['현재가'].apply(remove_comma)
+print(df)
+print(df.dtypes)
+
+df = df.astype({'현재가': np.int64})
+print(df)
+print(df.dtypes)
